a5fb3ffacc6459b7027e53343d10548170f42c24
Katya Saint-Amand <katya.saintamand+git@gmail.com>
2023-03-15 18:22:07 +0000

    updated application to non-debug mode

--- application.py
+++ application.py
@@ -532,8 +532,8 @@ def render_page_content(pathname):
 
 app.config['suppress_callback_exceptions']=True
 
-# if __name__=='__main__':
-#      application.run(host='0.0.0.0', port='8080')
+if __name__=='__main__':
+     application.run(host='0.0.0.0', port='8080')
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
